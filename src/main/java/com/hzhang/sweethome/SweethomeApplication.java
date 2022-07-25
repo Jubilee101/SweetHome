89d154f1e075c6505c8b193314ffe9b196478de6
Jubilee101 <haoranz4@andrew.cmu.edu>
2022-07-25 01:13:06 -0400

    first commit

---
+++ src/main/java/com/hzhang/sweethome/SweethomeApplication.java
@@ -0,0 +1,13 @@
+package com.hzhang.sweethome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SweethomeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SweethomeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hzhang/sweethome/SweethomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hzhang.sweethome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SweethomeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
