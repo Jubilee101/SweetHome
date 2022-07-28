c4778bf268d0c1693e4cd325bbdb3b8b8ff74292
Jubilee101 <haoranz4@andrew.cmu.edu>
2022-07-28 00:02:23 -0400

    add JwtFilter

--- src/main/java/com/hzhang/sweethome/config/SecurityConfig.java
+++ src/main/java/com/hzhang/sweethome/config/SecurityConfig.java
@@ -1,9 +1,11 @@
 package com.hzhang.sweethome.config;
 
+import com.hzhang.sweethome.filter.JwtFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.http.HttpMethod;
@@ -11,12 +13,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import javax.sql.DataSource;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private DataSource dataSource;
+
+    @Autowired
+    private JwtFilter jwtFilter;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -28,10 +34,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.POST,"/public_invoice/*").permitAll()
                 .antMatchers(HttpMethod.GET,"/public_invoice").permitAll()
                 .antMatchers(HttpMethod.GET,"/public_invoice/*").permitAll()
+                .antMatchers(HttpMethod.GET, "/unread_nums/*").permitAll()
+                .antMatchers(HttpMethod.GET, "/unread_nums").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .csrf()
                 .disable();
+
+        http.sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .addFilterBefore(jwtFilter,
+                        UsernamePasswordAuthenticationFilter.class);
     }
 
     @Bean
