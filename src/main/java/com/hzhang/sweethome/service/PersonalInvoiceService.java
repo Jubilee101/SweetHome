232b785ec6226c7d1fc7d51a38c5521dd03b3b4c
Jubilee101 <haoranz4@andrew.cmu.edu>
2022-07-29 14:08:39 -0400

    some revisions on personal invoice

--- src/main/java/com/hzhang/sweethome/service/PersonalInvoiceService.java
+++ src/main/java/com/hzhang/sweethome/service/PersonalInvoiceService.java
@@ -1,17 +1,26 @@
 package com.hzhang.sweethome.service;
 import com.hzhang.sweethome.exception.PersonalInvoiceNotExistException;
+import com.hzhang.sweethome.exception.UserNotExistException;
 import com.hzhang.sweethome.model.PersonalInvoice;
+import com.hzhang.sweethome.model.User;
 import com.hzhang.sweethome.repository.PersonalInvoiceReository;
+import com.hzhang.sweethome.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 public class PersonalInvoiceService {
     private PersonalInvoiceReository personalInvoiceReository;
+    private UserRepository userRepository;
     @Autowired
-    public PersonalInvoiceService(PersonalInvoiceReository personalInvoiceReository) {
+    public PersonalInvoiceService(PersonalInvoiceReository personalInvoiceReository,
+                                  UserRepository userRepository) {
         this.personalInvoiceReository = personalInvoiceReository;
+        this.userRepository = userRepository;
     }
+
     public List<PersonalInvoice> listByUserandType(String email, String type) throws PersonalInvoiceNotExistException {
         List<PersonalInvoice> invoiceList =  personalInvoiceReository.findByEmailandType(email,type);
         if(invoiceList==null){
@@ -21,8 +30,19 @@ public class PersonalInvoiceService {
         return invoiceList;
     }
 
-    public void add(PersonalInvoice personalInvoice){
-            personalInvoiceReository.save(personalInvoice);
+    public void add(String type, String text, String name, String room){
+        Optional<User> user = userRepository.findByRoomAndName(room, name);
+        if (!user.isPresent()) {
+            throw new UserNotExistException("Can not find user!");
+        }
+        PersonalInvoice personalInvoice = new PersonalInvoice
+                .Builder()
+                .setDate(LocalDate.now())
+                .setText(text)
+                .setType(type)
+                .setUser(user.get())
+                .build();
+        personalInvoiceReository.save(personalInvoice);
     }
 
 
