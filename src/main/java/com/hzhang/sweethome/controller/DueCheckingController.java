115088d8bf4d39bfdfe18c6ebdeab8bc34f2a8cd
Jubilee101 <haoranz4@andrew.cmu.edu>
2022-07-29 14:40:11 -0400

    add due checking controller

--- src/main/java/com/hzhang/sweethome/controller/DueCheckingController.java
+++ src/main/java/com/hzhang/sweethome/controller/DueCheckingController.java
@@ -1,4 +1,23 @@
 package com.hzhang.sweethome.controller;
 
+import com.hzhang.sweethome.service.DueCheckingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@RestController
 public class DueCheckingController {
+    private DueCheckingService dueCheckingService;
+
+    @Autowired
+    public DueCheckingController(DueCheckingService service){
+        dueCheckingService = service;
+    }
+
+    @PostMapping(value = "/due")
+    public void checkDue(Principal principal) {
+        dueCheckingService.checkDue(principal.getName());
+    }
 }
