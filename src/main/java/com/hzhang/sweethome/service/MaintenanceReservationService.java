7fc120c3224a02680f4b3e0b7d2f153a63641bff
Jubilee101 <haoranz4@andrew.cmu.edu>
2022-08-02 17:22:06 -0400

    change resident to user in maintenance reservations

--- src/main/java/com/hzhang/sweethome/service/MaintenanceReservationService.java
+++ src/main/java/com/hzhang/sweethome/service/MaintenanceReservationService.java
@@ -1,10 +1,12 @@
 package com.hzhang.sweethome.service;
 
 import com.hzhang.sweethome.exception.MaintenanceReservationNotFoundException;
+import com.hzhang.sweethome.exception.UserNotExistException;
 import com.hzhang.sweethome.model.MaintenanceImage;
 import com.hzhang.sweethome.model.MaintenanceReservation;
 import com.hzhang.sweethome.model.User;
 import com.hzhang.sweethome.repository.MaintenanceReservationRepository;
+import com.hzhang.sweethome.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -12,58 +14,72 @@ import org.springframework.web.multipart.MultipartFile;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
 public class MaintenanceReservationService {
     private MaintenanceReservationRepository maintenanceReservationRepository;
+    private UserRepository userRepository;
     private ImageStorageService imageStorageService;
 
     @Autowired
-    public MaintenanceReservationService(MaintenanceReservationRepository maintenanceReservationRepository){
+    public MaintenanceReservationService(MaintenanceReservationRepository maintenanceReservationRepository,
+                                         ImageStorageService imageStorageService,
+                                         UserRepository userRepository){
         this.maintenanceReservationRepository = maintenanceReservationRepository;
         this.imageStorageService = imageStorageService;
+        this.userRepository = userRepository;
     }
 
-    public void add(MaintenanceReservation maintenanceReservation, MultipartFile[] images){
+    public void add(String email, String description, MultipartFile[] images){
+        Optional<User> user = userRepository.findById(email);
+        if (!user.isPresent()) {
+            throw new UserNotExistException("Cannot find user!");
+        }
+        User resident = user.get();
+        MaintenanceReservation maintenanceReservation = new MaintenanceReservation.Builder()
+                .setDescription(description)
+                .setUser(new User.Builder().setEmail(email).build())
+                .build();
+
         List<String> mediaLinks = Arrays.stream(images).parallel().map(image -> imageStorageService.save(image)).collect(Collectors.toList());
         List<MaintenanceImage> maintenanceImageList = new ArrayList<>();
         for(String mediaLink : mediaLinks){
             maintenanceImageList.add(new MaintenanceImage(mediaLink, maintenanceReservation));
         }
         maintenanceReservation.setMaintenanceImages(maintenanceImageList);
+
         maintenanceReservationRepository.save(maintenanceReservation);
     }
 
-
-    // 为什么是room和name，不是id和name ？
-    public MaintenanceReservation findByRoomAndName(String room, String name) throws MaintenanceReservationNotFoundException{
-        MaintenanceReservation maintenanceReservation = maintenanceReservationRepository.findByRoomAndName(room, new User.Builder().setName(name).build());
-        return maintenanceReservation;
-    }
-
-    public List<MaintenanceReservation> listAll() throws MaintenanceReservationNotFoundException{
-        List<MaintenanceReservation> allMaintenanceReservation = maintenanceReservationRepository.findAll();
-        allMaintenanceReservation.sort((o1, o2) ->{
-            if (o1.getDate().equals(o2.getDate())) {
-                return -1 * o1.getStartTime().compareTo(o2.getStartTime());
-            }
-            return -1 * o1.getDate().compareTo(o2.getDate());
-        });
-        return allMaintenanceReservation;
-    }
-    /*
-    public void add(MaintenanceReservation maintenanceReservation){
-        maintenanceReservationRepository.save(maintenanceReservation);
-    }
-    /*
-    public void delete(String room, String name) throws MaintenanceReservationNotFoundException{
-        MaintenanceReservation maintenanceReservation = maintenanceReservationRepository.findByRoomAndName(room, new User.Builder().setName(name).build());
-        if(maintenanceReservation == null){
-            throw new MaintenanceReservationNotFoundException("Maintenance reservation does not exist");
+    public List<MaintenanceReservation> findByUser(String email) throws MaintenanceReservationNotFoundException{
+        Optional<User> user = userRepository.findById(email);
+        if (!user.isPresent()) {
+            throw new UserNotExistException("Cannot find user!");
         }
-        maintenanceReservationRepository.deleteById(maintenanceReservation.getId());    // ???
+        List<MaintenanceReservation> reservations = maintenanceReservationRepository.findByUser(user.get());
+        if (!reservations.isEmpty()) {
+            reservations.sort((MaintenanceReservation o1, MaintenanceReservation o2) -> {
+                if (o1.getDate() == null && o1.getDate() == null) {
+                    return 0;
+                }
+                else if (o1.getDate() == null) {
+                    return -1;
+                }
+                else if (o2.getDate() == null) {
+                    return 1;
+                }
+                else {
+                    if (o1.getDate().equals(o2.getDate())) {
+                        return -1 * o1.getStartTime().compareTo(o2.getStartTime());
+                    }
+                    else {
+                        return -1 * o1.getDate().compareTo(o2.getDate());
+                    }
+                }
+            });
+        }
+        return reservations;
     }
-     */
-
 }
