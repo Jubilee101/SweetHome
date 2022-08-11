bf4219711b6946854f73d5f4d557916b8c1ef387
Jubilee101 <haoranz4@andrew.cmu.edu>
2022-08-11 14:34:24 -0400

    finally solve the msg lose in long polling

--- src/main/java/com/hzhang/sweethome/controller/PollingController.java
+++ src/main/java/com/hzhang/sweethome/controller/PollingController.java
@@ -4,6 +4,7 @@ import com.hzhang.sweethome.model.DeferredRequestList;
 import com.hzhang.sweethome.model.InvoiceType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.async.DeferredResult;
@@ -23,5 +24,12 @@ public class PollingController {
     public DeferredResult<String> watch(@RequestParam(name = "type") String type
             , Principal principal) {
         return deferredRequestList.watch(principal.getName(), type);
+    }
+    // each person take one path so that there's no jam
+    @GetMapping(value = "/watch/{email}")
+    public DeferredResult<String> watchMsg(@PathVariable("email") String email,
+                                           Principal principal) {
+        System.out.println(email + " send msg watch request");
+        return deferredRequestList.watch(principal.getName(), "MESSAGE");
     }
 }
